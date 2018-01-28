f0fbd68315df6e116c4d3dca299e7be4c244d739
Hart Ungalled <przegiegaknieje@gmail.com>
2018-01-28 18:34:12 +0100

    Fix data type in entity class

--- src/main/java/com/CarRentProject/models/User.java
+++ src/main/java/com/CarRentProject/models/User.java
@@ -23,6 +23,14 @@ public class User {
         this.lastname = lastname;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
