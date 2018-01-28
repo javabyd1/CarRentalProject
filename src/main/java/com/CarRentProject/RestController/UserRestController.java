2cb8d59fb0c6a935311c1519b7310835329aec58
MateuszD <mateuszdeptula1@gmail.com>
2018-01-28 12:33:02 +0100

    add rest controller
    Rest Controller has method post in which we add new user entity

---
+++ src/main/java/com/CarRentProject/RestController/UserRestController.java
@@ -0,0 +1,20 @@
+package com.CarRentProject.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "users")
+public class UserRestController {
+
+    @Autowired
+    private UsersServiceImpl usersService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void addNewUser(User user){
+        usersService.addUser(user);
+    }
+    
+}
