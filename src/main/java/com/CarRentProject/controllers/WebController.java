1d23e76c1ceb3575417071161e27d955eddfca11
Hart Ungalled <przegiegaknieje@gmail.com>
2018-01-31 15:33:22 +0100

    Add feature to save user to db via register page

--- src/main/java/com/CarRentProject/controllers/WebController.java
+++ src/main/java/com/CarRentProject/controllers/WebController.java
@@ -1,12 +1,21 @@
 package com.CarRentProject.controllers;
 
 import com.CarRentProject.models.User;
+import com.CarRentProject.service.UsersServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class WebController {
+
+    @Autowired
+    UsersServiceImpl usersService;
 
     @GetMapping("/login")
     public ModelAndView showLoginPage() {
@@ -20,6 +29,12 @@ public class WebController {
         ModelAndView mav = new ModelAndView("register");
         mav.addObject("user", new User() );
         return mav;
+    }
+
+    @PostMapping("/register")
+    public String saveNewUser(@ModelAttribute("user") @Valid User user) {
+        usersService.saveUser(user);
+        return "redirect:/users";
     }
 
 }
