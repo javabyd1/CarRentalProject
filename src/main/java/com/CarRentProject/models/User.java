fb4b3abeb77920d12c39d26e18392bd7510207e4
MateuszD <mateuszdeptula1@gmail.com>
2018-02-03 14:52:59 +0100

    add tests and changed user model a bit

--- src/main/java/com/CarRentProject/models/User.java
+++ src/main/java/com/CarRentProject/models/User.java
@@ -3,16 +3,7 @@ package com.CarRentProject.models;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Set;
@@ -43,7 +34,7 @@ public class User {
     private Set<Roles> rolesSet;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private List<Car>listCar;
+    private List<Car> listCar;
 
     public User() {
     }
