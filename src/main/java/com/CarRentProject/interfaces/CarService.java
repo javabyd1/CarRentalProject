b41f7880f40bab2bca2844dadd836544efb0123f
Tomek <29851015+TomaszJava@users.noreply.github.com>
2018-02-08 21:36:18 +0100

    simplify and refactor car model, service and repository

--- src/main/java/com/CarRentProject/interfaces/CarService.java
+++ src/main/java/com/CarRentProject/interfaces/CarService.java
@@ -1,18 +1,19 @@
 package com.CarRentProject.interfaces;
 
 import com.CarRentProject.models.Car;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 
 public interface CarService {
 
-    List<Car>getAllCars();
+    List<Car> getAllCars();
+
     void saveCar(Car car);
+
     Car getCarbyBrand(String brand);
+
     Car getCarByModel(String model);
+
     Car getCarByYearOfProduction(int yearOfProduction);
-    Car getCarByPriceRentPerDay(int priceRentPerDay);
-    Car getCarByIsRented(boolean isRented);
 }
--- src/main/java/com/CarRentProject/repository/CarRepository.java
+++ src/main/java/com/CarRentProject/repository/CarRepository.java
@@ -12,8 +12,6 @@ public interface CarRepository extends JpaRepository<Car, Long> {
     Car findAllByBrand(String brand);
     Car findAllByModel(String model);
     Car findAllByYearOfProduction(int yearOfProduction);
-    Car findAllByPriceRentPerDay(int priceRentPerDay);
-    Car findAllByIsRented(boolean isRented);
 
     @Override
     List<Car> findAll();
