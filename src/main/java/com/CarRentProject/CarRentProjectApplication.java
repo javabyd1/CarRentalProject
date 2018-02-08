e6467da93a9df306e8026c3131b8f462d770775a
Tomek <29851015+TomaszJava@users.noreply.github.com>
2018-02-08 20:55:11 +0100

    add temporary user

--- src/main/java/com/CarRentProject/CarRentProjectApplication.java
+++ src/main/java/com/CarRentProject/CarRentProjectApplication.java
@@ -1,5 +1,8 @@
 package com.CarRentProject;
 
+import com.CarRentProject.models.User;
+import com.CarRentProject.service.UsersServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +10,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarRentProjectApplication implements CommandLineRunner {
 
+	@Autowired
+    UsersServiceImpl usersService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CarRentProjectApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		User admin = new User("admin", "123", "admin", "admin");
+		usersService.saveUser(admin);
 	}
 }
