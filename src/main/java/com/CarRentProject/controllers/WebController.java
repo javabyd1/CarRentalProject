e93ce5a77d87449d51f474575da48009f0acd296
Tomek <29851015+TomaszJava@users.noreply.github.com>
2018-02-09 10:25:15 +0100

    add mappings for home and my cars pages

--- src/main/java/com/CarRentProject/controllers/WebController.java
+++ src/main/java/com/CarRentProject/controllers/WebController.java
@@ -11,11 +11,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Controller
+@SessionAttributes("user")
 public class WebController {
 
     @Autowired
@@ -41,7 +44,7 @@ public class WebController {
     @PostMapping("/register")
     public String saveNewUser(@ModelAttribute("user") @Valid User user) {
         usersService.saveUser(user);
-        return "redirect:/users";
+        return "redirect:/login";
     }
 
     @PostMapping("/save-car")
@@ -51,26 +54,40 @@ public class WebController {
     }
 
     @PostMapping("/login")
-    public String checkLoginData(@ModelAttribute("user") @Valid User user) {
-        User dbUser = usersService.getUserByLoginAndPassword(user.getLogin(), user.getPassword());
-        if (dbUser != null)
-            return "redirect:/login-ok";
-        else
-            return "redirect:/login-error";
+    public String checkLoginData(HttpSession session, @ModelAttribute("user") @Valid User loginData) {
+        User user = usersService.getUserByLoginAndPassword(loginData.getLogin(), loginData.getPassword());
+        if (user != null) {
+            session.setAttribute("user", user);
+            return "redirect:/my-cars";
+        } else {
+            return "redirect:/login?error=user_not_found";
+        }
     }
-
-//    @GetMapping("/carsForRent")
-//    public ModelAndView showListOfCarsForRent() {
-//        ModelAndView modelAndView = new ModelAndView("carsForRent");
-//        modelAndView.addObject("car", new Car());
-//        return modelAndView;
-   // }
 
     @RequestMapping(value = "cars", method = RequestMethod.GET)
     public ModelAndView cars() {
         ModelAndView mav = new ModelAndView("cars");
         mav.addObject("cars", carsService.getAllCars());
         mav.addObject("car", new Car());
+        return mav;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ModelAndView home() {
+        ModelAndView mav = new ModelAndView("home");
+        mav.addObject("cars", carsService.getAllCars());
+        return mav;
+    }
+
+    @RequestMapping(value = "my-cars", method = RequestMethod.GET)
+    public ModelAndView listOfUsersCars(HttpSession session) {
+        User user = (User) session.getAttribute("user");
+
+        ModelAndView mav = new ModelAndView("mycars");
+        mav.addObject("allCars", carsService.getAllCars());
+        mav.addObject("userCars", user.getListOfCars());
+        mav.addObject("car", new Car());
+        mav.addObject("user", user);
         return mav;
     }
 
