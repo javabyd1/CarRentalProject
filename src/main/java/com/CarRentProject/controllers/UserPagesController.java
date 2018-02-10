26a0cddae10cad4c3e8329b7240781b1988c4569
Anna <annajakub@wp.pl>
2018-02-10 14:00:19 +0100

    aproved view - redirection to rent-car

--- src/main/java/com/CarRentProject/controllers/UserPagesController.java
+++ src/main/java/com/CarRentProject/controllers/UserPagesController.java
@@ -7,12 +7,15 @@ import com.CarRentProject.service.UsersServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 @Controller
 @SessionAttributes("user")
